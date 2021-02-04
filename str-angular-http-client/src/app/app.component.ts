28592b46844e1eb0376d497e9da97e69a27d38db
ethuringer <ethuringer@gmail.com>
2021-02-04 18:38:56 +0100

    angular-http practice

---
+++ str-angular-http-client/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './model/user';
+import { UserService } from './service/user.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'str-angular-http-client';
+
+  userList$: Observable<User[]> = this.userService.getAll();
+
+  constructor(
+    private userService: UserService,
+  ) {}
+}
